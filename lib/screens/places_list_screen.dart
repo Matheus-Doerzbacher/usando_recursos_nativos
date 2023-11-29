524d3561ee8d2b5996e8b84312528537a4552195
Matheus Doerzbacher <matheus.doer@gmail.com>
2023-11-29 20:11:25 -0300

    editada a tela principal para aparecer os Places na lista

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:usando_recursos_nativos/providers/grate_places.dart';
 import 'package:usando_recursos_nativos/utils/app_routes.dart';
 
 class PlacesListScreen extends StatelessWidget {
@@ -18,8 +20,27 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GratePlaces>(
+        child: const Center(
+          child: Text("Nenhuma localização cadastrada"),
+        ),
+        builder: (ctx, gratePlace, child) => gratePlace.itemsCount == 0
+            ? child!
+            : ListView.builder(
+                itemCount: gratePlace.itemsCount,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                      gratePlace.itemByIndex(i).image,
+                    ),
+                  ),
+                  title: Text(
+                    gratePlace.items[i].title,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  onTap: () {},
+                ),
+              ),
       ),
     );
   }
