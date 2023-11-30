6b3c951312efe575cc2918a4f682296402e5c239
Matheus Doerzbacher <matheus.doer@gmail.com>
2023-11-30 09:40:38 -0300

    Inserida a opção para escolher um ponto no mapa

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:usando_recursos_nativos/screens/map_screen.dart';
 import 'package:usando_recursos_nativos/utils/location_util.dart';
@@ -25,14 +26,14 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    final LatLng? selectedPosition = await Navigator.of(context).push(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (ctx) => const MapScreen(),
       ),
     );
 
-    if (selectedLocation == null) {
+    if (selectedPosition == null) {
       return;
     }
   }
